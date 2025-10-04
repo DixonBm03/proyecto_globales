810030d8d7c92f412ce36e3d125b965f3a3b217e
Giancarlo Chaves <gchaves@officespacesoftware.com>
2025-10-03 23:35:46 -0600

    Update MOCKAPI_URL_USERS in mockAqi.js for improved readability

--- src/data/ mockAqi.js
+++ src/data/ mockAqi.js
@@ -2,4 +2,5 @@ export const aqiNow = 82; // valor AQI ficticio
 export const pm25 = 32; // μg/m³
 export const pm10 = 55; // μg/m³
 
-export const MOCKAPI_URL_USERS = 'https://68dc34707cd1948060a9ba20.mockapi.io/api/';
+export const MOCKAPI_URL_USERS =
+  'https://68dc34707cd1948060a9ba20.mockapi.io/api/';
